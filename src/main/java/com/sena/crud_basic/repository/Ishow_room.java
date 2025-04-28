ef17cc15a56c0e8c75d9a6938f0934a0971d3ef6
Johan Cuellar <cuellarjohan.07@gmail.com>
2025-04-28 16:56:36 -0500

    Delete some files

--- src/main/java/com/sena/crud_basic/repository/Ishow_room.java
+++
@@ -1,9 +0,0 @@
-package com.sena.crud_basic.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sena.crud_basic.model.show_room;
-
-public interface Ishow_room extends JpaRepository<show_room, Integer>{
-    
-}
